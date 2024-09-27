96c8773b9efb94987f5d9ded67cc26dc699a4f06
Anurag Singh <theanuragsingh911@gmail.com>
2024-09-27 12:08:52 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+genai.configure(api_key=os.getenv('GOOGLE-API-KEY'))
+# Desinging the Page
+st.title('Image to Text Application')
+user_input=st.text_input('Input Prompt')
+upload_file=st.file_uploader('Upload the Image....',type=['jpg','jpeg','png'])
+
+# Display the image on the page
+from PIL import Image
+img=''
+if upload_file is not None:
+    img=Image.open(upload_file)
+    st.image(img,caption='Uploaded_Image',use_column_width=True)
+    
+    
+## Function for evaluating the image and annotating it.
+
+def gemini_response(user_input,img):
+    model=genai.GenerativeModel("gemini-1.5-flash")
+    if user_input!="":
+        response=model.generate_content([user_input,img])    
+        
+    else:
+        response=model.generate_content(img)
+    return response.text  
+
+# Creating submit button and MAP the GenAI function  
+submit=st.button("SUBMIT")
+
+if submit:
+    response=gemini_response(user_input=user_input,img=img)
+    st.subheader("The Response is :")
+    st.write(response)
